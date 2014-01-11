e0ec8853d8033c66a861b2cbfe81481ef141cf67
Hereward <hereward@planetonline.com.au>
2014-01-11 22:20:07 +0700

    missing navigation PUT BACK

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -1,120 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<!-- Basic Page Needs
-		================================================== -->
-		<meta charset="utf-8" />
-		<title>
-			@section('title')
-			Laravel 4 Sample Site
-			@show
-		</title>
-		<meta name="keywords" content="your, awesome, keywords, here" />
-		<meta name="author" content="Jon Doe" />
-		<meta name="description" content="Lorem ipsum dolor sit amet, nihil fabulas et sea, nam posse menandri scripserit no, mei." />
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8" />
+    <title>
+        @section('title')
+        Phone Home
+        @show
+    </title>
+    <meta name="keywords" content="phone, home, cheap, easy" />
+    <meta name="author" content="Hereward Fenton" />
+    <meta name="description" content="A convenient, cheap and flexible way to keep in touch with your contacts while overseas." />
 
-		<!-- Mobile Specific Metas
-		================================================== -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!-- CSS
-		================================================== -->
-        {{ Basset::show('public.css') }}
+    <!-- CSS
+    ================================================== -->
+    {{ Basset::show('public.css') }}
 
-        <link href="/assets/css/custom.css" type="text/css" rel="stylesheet">
+    <link href="/assets/css/custom.css" type="text/css" rel="stylesheet">
 
-		<style>
-		@section('styles')
+    <style>
+        @section('styles')
 		@show
-		</style>
+    </style>
 
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-		<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 
-		<!-- Favicons
-		================================================== -->
-		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{{ asset('assets/ico/apple-touch-icon-144-precomposed.png') }}}">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{{ asset('assets/ico/apple-touch-icon-114-precomposed.png') }}}">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}}">
-		<link rel="apple-touch-icon-precomposed" href="{{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}}">
-		<link rel="shortcut icon" href="{{{ asset('assets/ico/favicon.png') }}}">
-	</head>
+    <!-- Favicons
+    ================================================== -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{{ asset('assets/ico/apple-touch-icon-144-precomposed.png') }}}">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{{ asset('assets/ico/apple-touch-icon-114-precomposed.png') }}}">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}}">
+    <link rel="apple-touch-icon-precomposed" href="{{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}}">
+    <link rel="shortcut icon" href="{{{ asset('assets/ico/favicon2.png') }}}">
+</head>
 
-	<body>
-		<!-- To make sticky footer need to wrap in a div -->
-		<div id="wrap">
-		<!-- Navbar -->
-		<div class="navbar navbar-default navbar-inverse navbar-fixed-top">
-			 <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav">
-						<li {{ (Request::is('/') ? ' class="active"' : '') }}><a href="{{{ URL::to('') }}}">Home</a></li>
-					</ul>
+<body>
+<!-- To make sticky footer need to wrap in a div -->
+<div id="wrap">
+    <!-- Navbar -->
+    @include('site/navigation')
 
-                    <ul class="nav navbar-nav pull-right">
-                        @if (Auth::check())
-                        @if (Auth::user()->hasRole('admin'))
-                        <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
-                        @endif
-                        <li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
-                        <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
-                        @else
-                        <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
-                        <li {{ (Request::is('user/register') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
-                        @endif
-                    </ul>
-					<!-- ./ nav-collapse -->
-				</div>
-			</div>
-		</div>
-		<!-- ./ navbar -->
+    <!-- ./ navbar -->
 
-		<!-- Container -->
-		<div class="container">
-            <div class="jumbotron">
-                <div class="row">
-                    <!-- header -->
+    <!-- Container -->
+    <div class="container">
+        <div class="jumbotron">
+            <div class="row">
+                <!-- header -->
 
-                    @include('site/header')
+                @include('site/header')
 
 
-                    <!-- ./ header -->
-                </div>
+                <!-- ./ header -->
             </div>
-			<!-- Notifications -->
-			@include('notifications')
-			<!-- ./ notifications -->
+        </div>
 
-			<!-- Content -->
-			@yield('content')
-			<!-- ./ content -->
-		</div>
-		<!-- ./ container -->
+        <div class="row">
+            <!-- Notifications -->
+            @include('notifications')
+            <!-- ./ notifications -->
+        </div>
 
-		<!-- the following div is needed to make a sticky footer -->
-		<div id="push"></div>
-		</div>
-		<!-- ./wrap -->
+        <div class="row">
+            <!-- Content -->
+            @yield('content')
+            <!-- ./ content -->
+        </div>
+    </div>
+    <!-- ./ container -->
+
+    <!-- the following div is needed to make a sticky footer -->
+    <div id="push"></div>
+</div>
+<!-- ./wrap -->
 
 
-	    <div id="footer">
-	      <div class="container">
-	        <p class="muted credit">Laravel 4 Starter Site on <a href="https://github.com/andrew13/Laravel-4-Bootstrap-Starter-Site">Github</a>.</p>
-	      </div>
-	    </div>
+<div id="footer">
+    <div class="container">
+        <p class="muted credit ph_footer">PhoneHome.asia - all rights reserved.</p>
+    </div>
+</div>
 
-		<!-- Javascripts
-		================================================== -->
-        {{ Basset::show('public.js') }}
-	</body>
+<!-- Javascripts
+================================================== -->
+{{ Basset::show('public.js') }}
+</body>
 </html>
