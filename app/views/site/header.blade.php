edaf4d6a4eb3f6610d920cecb7d803bc6461ffd0
Hereward <hereward@planetonline.com.au>
2014-01-12 10:55:04 +0700

    fiddle

--- app/views/site/header.blade.php
+++ app/views/site/header.blade.php
@@ -1,7 +1,7 @@
 
 <div class=col-lg-12>
 
-    <h1>PhoneHome.Asia</h1>
+    <h1>PhoneHome</h1>
     <p>A convenient, cheap and flexible way to keep in touch with your contacts while overseas.</p>
     <p><a class="btn btn-primary btn-lg">Take the Tour Now</a></p>
 </div>
