6a6de9beeadb834fdec49b1410bea7981a3e15f3
Hereward <hereward@planetonline.com.au>
2014-01-12 14:37:21 +0700

    fiddle

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -195,7 +195,7 @@ class ProfileController extends \BaseController {
                 ));
 
             // redirect
-            Session::flash('message', 'Successfully created profile. A new profile request has been lodged in our system. This request is pending and should be active within 24
+            Session::flash('message', 'A new profile request has been lodged in our system. This request is pending and should be active within 24
             hours.');
             return Redirect::to('profile');
         }
