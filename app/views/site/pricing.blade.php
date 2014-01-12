5f775a7c00b871819d7873a72658250f976d8850
Hereward <hereward@planetonline.com.au>
2014-01-12 13:21:23 +0700

    Added some new files

---
+++ app/views/site/pricing.blade.php
@@ -0,0 +1,20 @@
+@extends('site.layouts.default_lite')
+
+@section('content')
+
+
+
+<div class="row">
+
+    <div class="jumbotron">
+
+        <h1>PRICING</h1>
+        <p>No content for this page!</p>
+
+
+    </div>
+
+
+</div>
+
+@stop
---
+++ app/views/site/quickstart.blade.php
@@ -0,0 +1,20 @@
+@extends('site.layouts.default_lite')
+
+@section('content')
+
+
+
+<div class="row">
+
+    <div class="jumbotron">
+
+        <h1>QUICK START</h1>
+        <p>No content for this page!</p>
+
+
+    </div>
+
+
+</div>
+
+@stop
