0668182962f4b2b2a2688522f82a5e78bc23fea2
Hereward <hereward@planetonline.com.au>
2014-01-12 09:53:35 +0700

    added delphinium (staging) to environments

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
     'local' => array('winston-ssd-VirtualBox'), // Change this to your local machine hostname.
-    'staging' => array('your-staging-machine-name'),
+    'staging' => array('delphinium'),
     'production' => array('your-production-machine-name'),
 
 ));
